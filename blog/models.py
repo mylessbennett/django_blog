20bc6028aa70f9b841c99e39c611a31c0fe09dff
Myles Bennett <myles.bennett@hotmail.com>
2019-03-04 08:52:10 -0500

    added article class to models

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    draft = models.BooleanField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=255)
